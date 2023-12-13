a1953eab0432e3032ab73d09ca997728a2913eea
Gil-CP <gilmarac3po@gmail.com>
2023-12-13 19:25:20 -0300

    1157
    Recebe um valor inteiro e calcula todos os seus divisores

---
+++ Iniciante/1157.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+    scanf("%d", &numero);
+
+    for(int i = 1; i <= numero; i++)
+    {
+        if(numero % i == 0)
+            printf("%d\n", i);
+    }
+}
