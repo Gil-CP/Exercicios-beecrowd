e50c804dfb28abdaca261431885d5e12f1698667
Gil-CP <gilmarac3po@gmail.com>
2023-12-13 20:10:11 -0300

    1007
    Lê quatro valores inteiros e imprime a diferença dado uma fórmula.

---
+++ Iniciante/1007.c
@@ -0,0 +1,11 @@
+/*Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    printf("DIFERENCA = %d\n", (a * b) - (c * d));
+}
