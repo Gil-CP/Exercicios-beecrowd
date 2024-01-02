f2c511bd82493ce6d76af3526dd5ef3e75898f33
Gil-CP <101299282+Gil-CP@users.noreply.github.com>
2024-01-02 18:14:28 -0300

    Add files via upload
    Calcula um fatorial

---
+++ Iniciante/1072.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, fatorial = 1;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+    {
+        fatorial *= (n - i);
+    }
+    printf("%d\n", fatorial);
+}
